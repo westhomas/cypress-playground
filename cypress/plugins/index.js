b58714ce5c4e524f92c3ec3758d96dd7c6634dbb
Wes Thomas <westhomas@gmail.com>
2020-02-07 10:08:33 -0600

    Following cypress-webpack-preprocessor's readme

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,34 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const webpack = require('@cypress/webpack-preprocessor')
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+
+    const options = {
+        // send in the options from your webpack.config.js, so it works the same
+        // as your app's code
+        // webpackOptions: require('../../webpack.config'),
+        webpackOptions: {
+            module: {
+                rules: [
+                    {
+                        test: /\.js?$/,
+                        exclude: [/node_modules/],
+                        use: [{
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['babel-preset-env'],
+                            },
+                        }],
+                    },
+                ],
+            },
+        },
+        watchOptions: {},
+    }
+
+    on('file:preprocessor', webpack(options))
 }
